198e324a50014039b596b1213e1dedea262a22b9
SubZero13 <60981763+Aniket13-cpu@users.noreply.github.com>
2021-11-22 14:40:08 -0500

    Add files via upload

---
+++ 191. Number of 1 Bits.java
@@ -0,0 +1,17 @@
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int count = 0;
+        String s = Integer.toBinaryString(n);
+        int i = 0;
+        while(i < s.length())
+        {
+            if(s.charAt(i) == '1')
+            {
+                count++;
+            }
+            i++;
+        }
+        return count;
+    }
+}
