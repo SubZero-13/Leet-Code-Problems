27069a281bc64dabd8389acf41ad9ca54da08077
SubZero13 <60981763+Aniket13-cpu@users.noreply.github.com>
2022-01-15 21:57:33 +0530

    Add files via upload

---
+++ 1588 Sum of All Odd Length Subarrays.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int sumOddLengthSubarrays(int[] arr) {
+        int sum = 0;
+        for(int i=0; i<arr.length; i++){
+            sum += ( (i + 1) * (arr.length - i) + 1) / 2 * arr[i]; 
+        }
+        return sum;
+    }
+}
