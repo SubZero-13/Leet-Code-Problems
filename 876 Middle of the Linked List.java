62f268dbc37ce3ef4c744e0b92baa0d202d3270e
SubZero13 <60981763+Aniket13-cpu@users.noreply.github.com>
2022-01-28 16:51:41 +0530

    Add files via upload

---
+++ 876 Middle of the Linked List.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode res = head;
+        ListNode curr = head;
+        while(curr != null && curr.next != null)
+        {
+            res = res.next;
+            curr = curr.next.next;
+        }
+        return res;
+    }
+}
