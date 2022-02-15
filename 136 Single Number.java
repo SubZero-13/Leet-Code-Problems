dace33059d979a1fac8716612e71fd975fb4da2f
SubZero13 <60981763+Aniket13-cpu@users.noreply.github.com>
2022-02-15 22:38:02 +0530

    Add files via upload

--- 136 Single Number.java
+++ 136 Single Number.java
@@ -1,10 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int result = 0;
-        for(int i = 0; i < nums.length; i++)
+        for (int i = 0; i<nums.length; i++)
         {
-            result ^= nums[i];
+		    result ^=nums[i];
         }
-        return result;
+	    return result;
     }
 }
