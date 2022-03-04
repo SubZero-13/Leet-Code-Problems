e6039047df0158bdf7a9d78f2cd86c00463239aa
SubZero13 <60981763+Aniket13-cpu@users.noreply.github.com>
2022-03-05 01:56:07 +0530

    Update 17 Letter Combinations of a Phone Number.java

--- 17 Letter Combinations of a Phone Number.java
+++ 17 Letter Combinations of a Phone Number.java
@@ -1,17 +1,22 @@
 class Solution {
+   public void getCombo(String keypad[], String s, int idx, String combo, List<String> ans) {
+        if(idx == s.length()) {
+            ans.add(combo);
+            return;
+        }
+        char currChar = s.charAt(idx);
+        String mapping = keypad[currChar-'0'];
+        for(int i = 0; i < mapping.length(); i++) {
+            getCombo(keypad, s, idx+1, combo+mapping.charAt(i), ans);
+        }
+    }
     public List<String> letterCombinations(String digits) {
-     LinkedList<String> ans = new LinkedList<String>();
-		if(digits.isEmpty()) return ans;
-		String[] mapping = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-		ans.add("");
-		for(int i =0; i<digits.length();i++){
-			int x = Character.getNumericValue(digits.charAt(i));
-			while(ans.peek().length()==i){
-				String t = ans.remove();
-				for(char s : mapping[x].toCharArray())
-					ans.add(t+s);
-			}
-		}
-		return ans;
+        List<String> ans = new ArrayList<>();
+        if(digits.length() == 0) {
+            return ans;
+        }
+        String keypad[] = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        getCombo(keypad, digits, 0, "", ans);
+        return ans;
     }
 }
