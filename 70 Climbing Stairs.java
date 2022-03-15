00cb0ae31b642a5c5d4f67a2056ec55f7b2874de
SubZero13 <60981763+Aniket13-cpu@users.noreply.github.com>
2022-03-16 02:37:20 +0530

    Update 70 Climbing Stairs.java

--- 70 Climbing Stairs.java
+++ 70 Climbing Stairs.java
@@ -1,17 +1,26 @@
 class Solution {
+    public int helper(int n, int arr[]) {
+        if(n == 0) {
+            return 1;
+        }
+        if(n < 0) {
+            return 0;
+        }
+        if(arr[n] != 0) {
+            return arr[n];
+        }
+        int way1 = helper(n-1, arr);
+        int way2 = helper(n-2, arr);
+        arr[n] = way1 + way2;
+        return way1 + way2;
+    }
     public int climbStairs(int n) {
-        int count = 1;
-		for (int i = 1; i <= n / 2; i++) {
-		count += calculateFactorial(n - (i * 2)+1, n - (i * 2) + i) / calculateFactorial(1, i);
-		}      
-        return count;
-	}
-
-	public static double calculateFactorial(int startNum, int endNum) {
-	
-		double factorial = 1;
-		for (int i = startNum; i <= endNum; i++)
-			factorial *= i;
-		return factorial;
+        if(n == 0) {
+            return 1;
+        }
+        if(n < 0) {
+            return 0;
+        }
+       return helper(n, new int[n+1]);
     }
 }
