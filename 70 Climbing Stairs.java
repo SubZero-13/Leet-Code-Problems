246ff9212611de356fe006584e1b812f3e9e3303
SubZero13 <60981763+Aniket13-cpu@users.noreply.github.com>
2022-03-16 02:46:36 +0530

    Update 70 Climbing Stairs.java

--- 70 Climbing Stairs.java
+++ 70 Climbing Stairs.java
@@ -1,26 +1,44 @@
+//PepCoding
 class Solution {
-    public int helper(int n, int arr[]) {
-        if(n == 0) {
-            return 1;
-        }
-        if(n < 0) {
-            return 0;
-        }
-        if(arr[n] != 0) {
-            return arr[n];
-        }
-        int way1 = helper(n-1, arr);
-        int way2 = helper(n-2, arr);
-        arr[n] = way1 + way2;
-        return way1 + way2;
-    }
+    // Using Memoization DP
+    // public int helper(int n, int arr[]) {
+    //     if(n == 0) {
+    //         return 1;
+    //     }
+    //     if(n < 0) {
+    //         return 0;
+    //     }
+    //     if(arr[n] != 0) {
+    //         return arr[n];
+    //     }
+    //     int way1 = helper(n-1, arr);
+    //     int way2 = helper(n-2, arr);
+    //     arr[n] = way1 + way2;
+    //     return way1 + way2;
+    // }
     public int climbStairs(int n) {
-        if(n == 0) {
-            return 1;
+       //  Using Memoization DP
+       //  if(n == 0) {
+       //      return 1;
+       //  }
+       //  if(n < 0) {
+       //      return 0;
+       //  }
+       // return helper(n, new int[n+1]);
+        
+        //Using Tabulation Mehod
+        
+        int arr[] = new int[n+1];
+        arr[0] = 1;
+        for(int i = 1; i < arr.length; i++) {
+            if(i == 1) {
+                arr[i] = arr[i-1];
+            }
+            else {
+                arr[i] = arr[i-1] + arr[i-2];
+            }
         }
-        if(n < 0) {
-            return 0;
-        }
-       return helper(n, new int[n+1]);
+        return arr[n];
+        
     }
 }
