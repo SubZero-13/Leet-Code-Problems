70cb3d6d71164430f041dd2b279d8406c6d176ee
SubZero13 <60981763+Aniket13-cpu@users.noreply.github.com>
2022-03-18 23:19:53 +0530

    Add files via upload

---
+++ 696 Count Binary Substrings.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countBinarySubstrings(String s) {
+         int cur = 1, pre = 0, res = 0;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) cur++;
+            else {
+                res += Math.min(cur, pre);
+                pre = cur;
+                cur = 1;
+            }
+        }
+        return res + Math.min(cur, pre);
+    }
+}
