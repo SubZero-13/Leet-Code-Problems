c3ab78b917cac75ed160153529499e6dfa4d2a3c
SubZero13 <60981763+Aniket13-cpu@users.noreply.github.com>
2022-04-09 19:46:53 +0530

    Add files via upload

---
+++ 1451 Rearrange Words in a Sentence.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String arrangeWords(String text) {
+        String []s = text.toLowerCase().split(" ");
+        Arrays.sort(s, (a,b) ->  a.length() - b.length());
+        String ans = String.join(" ", s);
+        return Character.toUpperCase(ans.charAt(0)) + ans.substring(1);
+    }
+        
+}
