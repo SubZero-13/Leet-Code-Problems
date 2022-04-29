869714f7f9f035c779d68100d79393c4d03350b6
Aniket Keshri <60981763+SubZero-13@users.noreply.github.com>
2022-04-29 23:10:32 +0530

    Add files via upload

---
+++ 1302 Deepest Leaves Sum.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int deepestLeavesSum(TreeNode root) {
+        int res = 0, i;
+        LinkedList<TreeNode> q = new LinkedList<TreeNode>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            for (i = q.size() - 1, res = 0; i >= 0; --i) {
+                TreeNode node = q.poll();
+                res += node.val;
+                if (node.right != null) q.add(node.right);
+                if (node.left  != null) q.add(node.left);
+            }
+        }
+        return res;
+    }
+}
