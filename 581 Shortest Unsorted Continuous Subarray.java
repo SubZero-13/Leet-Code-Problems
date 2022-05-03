2c2ffc12c87082665d4bf6a5a4756d0c0ea49ea1
Aniket Keshri <60981763+SubZero-13@users.noreply.github.com>
2022-05-03 23:11:04 +0530

    Add files via upload

---
+++ 581 Shortest Unsorted Continuous Subarray.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findUnsortedSubarray(int[] nums) {
+        int start = 0, end = -1;
+        int max = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            if(max > nums[i]) {
+                end = i;
+            }
+            else {
+                max = nums[i];
+            }
+        }
+        int min  = nums[nums.length-1];
+        for(int i = nums.length-2; i >= 0; i--) {
+            if(min < nums[i]) {
+                start = i;
+            }
+            else {
+                min = nums[i];
+            }
+        }
+        return end - start + 1;
+    }
+}
