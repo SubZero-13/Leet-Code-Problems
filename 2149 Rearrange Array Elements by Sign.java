24d60c71a419b6c1750d2b03b426409fd0c3e3e8
Aniket Keshri <60981763+SubZero-13@users.noreply.github.com>
2022-06-02 20:25:26 +0530

    Add files via upload

---
+++ 2149 Rearrange Array Elements by Sign.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        int ans[] = new int[nums.length];
+        int i = 0;
+        int j = 1;
+        for(int num:nums) {
+            if(num > 0) {
+                ans[i] = num;
+                i += 2;
+            }
+            else {
+                ans[j] = num;
+                j += 2;
+            }
+        }
+        return ans;
+        
+    }
+}
