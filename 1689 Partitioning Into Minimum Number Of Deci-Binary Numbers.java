662b07560ddf9035901a1f4563cfa5028242930c
Aniket Keshri <60981763+SubZero-13@users.noreply.github.com>
2022-06-27 23:11:06 +0530

    Update 1689 Partitioning Into Minimum Number Of Deci-Binary Numbers.java

--- 1689 Partitioning Into Minimum Number Of Deci-Binary Numbers.java
+++ 1689 Partitioning Into Minimum Number Of Deci-Binary Numbers.java
@@ -1,13 +1,18 @@
 class Solution {
     public int minPartitions(String n) {
-        int res = 0;
-        int max = 0;
-        char[] charArr = n.toCharArray();
-        for(char c: charArr)
-        {
-            max = Character.getNumericValue(c);
-            res = Math.max(max, res);
-        }
-        return res;
+        
+        // Method - 1
+        // int res = 0;
+        // int max = 0;
+        // char[] charArr = n.toCharArray();
+        // for(char c: charArr)
+        // {
+        //     max = Character.getNumericValue(c);
+        //     res = Math.max(max, res);
+        // }
+        // return res;
+        
+        // Method - 2
+        return n.chars().max().getAsInt() - '0';
     }
 }
