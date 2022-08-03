d82113edee6e01774ffabc2898f3fdad54880fbd
Aniket Keshri <60981763+SubZero-13@users.noreply.github.com>
2022-08-04 01:35:49 +0530

    Update 462 Minimum Moves to Equal Array Elements II.java

--- 462 Minimum Moves to Equal Array Elements II.java
+++ 462 Minimum Moves to Equal Array Elements II.java
@@ -1,15 +1,13 @@
 class Solution {
     public int minMoves2(int[] nums) {
-          if (nums == null || nums.length == 0) {
-            return 0;
+        Arrays.sort(nums);
+        int i = 0, j = nums.length-1;
+        int count = 0;
+        while(i < j) {
+            count += nums[j] - nums[i];
+            i++;
+            j--;
         }
-        Arrays.sort(nums); // sort the array
-        int n = nums.length; // length of array
-        int mid = n / 2; // middle of array
-        int ans = 0; // answer
-        for (int i = 0; i < nums.length; i++) {
-            ans += Math.abs(nums[i] - nums[mid]); // diff between mid and curr index to make equal
-        }
-        return ans;
+        return count;
     }
 }
