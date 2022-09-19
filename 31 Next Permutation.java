49f5750b4493a394047225953e71259e43def5d4
Aniket Keshri <60981763+SubZero-13@users.noreply.github.com>
2022-09-20 05:10:08 +0530

    Update 31 Next Permutation.java

--- 31 Next Permutation.java
+++ 31 Next Permutation.java
@@ -1,33 +1,24 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int n = nums.length;
-        if(n==1)
-            return;
+        if(nums == null || nums.length <= 1) return;
+        int i = nums.length-2;
+        while(i >= 0 && nums[i] >= nums[i+1]) i--;
         
-        int i=1;
-        int lastInc = -1;
-        while(i<n) {    //Find the peak of last ASC order
-            if(nums[i]>nums[i-1])
-                lastInc = i;
-            i+=1;
+        if(i >= 0) {
+            int j = nums.length-1;
+            while(j >= 0 && nums[j] <= nums[i]) j--;
+            swap(nums, i, j);
         }
-        
-        if(lastInc==-1) {   //Check if array is DSC
-            Arrays.sort(nums);
-            return;
-        }
-        //Find element in the range (nums[lastInc-1] to nums[lastInc]) to the right
-        int mn = nums[lastInc];
-        int index = lastInc;
-        for(i=lastInc;i<n;++i) {
-            if(nums[i]>nums[lastInc-1] && nums[i]<nums[index]) {
-                index = i;
-            }
-        }
-        int t = nums[lastInc-1];
-        nums[lastInc-1] = nums[index];
-        nums[index] = t;
-        Arrays.sort(nums,0+lastInc,nums.length);
+        reverse(nums, i+1, nums.length-1);
         
     }
+    private void swap(int arr[], int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    private void reverse(int arr[], int i, int j) {
+        while(i < j) swap(arr, i++, j--);
+    }
+    
 }
