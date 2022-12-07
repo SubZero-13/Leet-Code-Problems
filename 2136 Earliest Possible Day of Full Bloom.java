418eb3f97aa30bfbdf0f3d543536683b971462bd
Aniket Keshri <60981763+SubZero-13@users.noreply.github.com>
2022-12-07 23:06:47 +0530

    Add files via upload

---
+++ 2136 Earliest Possible Day of Full Bloom.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int earliestFullBloom(int[] plantTime, int[] growTime) {
+        int n = plantTime.length;
+        int[][] mat = new int[n][2];
+        for(int i = 0; i < n; i++) {
+            mat[i][0] = plantTime[i];
+            mat[i][1] = growTime[i];
+        }
+        Arrays.sort(mat, (a,b) -> b[1]-a[1]);
+        int bloomDay = 0, seedPlantingDay = 0;
+        for(int i = 0; i < n; i++) {
+            int ptime = mat[i][0];
+            int gtime = mat[i][1];
+            bloomDay = Math.max(bloomDay, seedPlantingDay+ptime+gtime);
+            seedPlantingDay = seedPlantingDay+ptime;
+        }
+        return bloomDay;
+    }
+}
