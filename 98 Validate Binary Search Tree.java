4390103c665168d603d9fb9894af63d0a6b8c4d5
Aniket Keshri <60981763+SubZero-13@users.noreply.github.com>
2022-12-31 23:38:17 +0530

    Add files via upload

---
+++ 98 Validate Binary Search Tree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        if(root == null) return true;
+        Stack<TreeNode> st = new Stack<>();
+        TreeNode pre = null;
+        while(root != null || !st.isEmpty()) {
+            while(root != null) {
+                st.push(root);
+                root = root.left;
+            }
+            root = st.pop();
+            if(pre != null && root.val <= pre.val) return false;
+            pre = root;
+            root = root.right;
+        } 
+        return true;
+    }
+}
