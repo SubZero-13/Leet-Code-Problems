32498d1925a2ca543de0c8687de50e729b905d6c
Aniket Keshri <60981763+SubZero-13@users.noreply.github.com>
2023-01-14 23:39:22 +0530

    Update 75 Sort Colors.java

--- 75 Sort Colors.java
+++ 75 Sort Colors.java
@@ -1,34 +1,25 @@
 class Solution {
+    // Dutch Man National Flag Algorithm
     public void sortColors(int[] nums) {
-        int countZero = 0;
-        int countOne = 0;
-        int countTwo = 0;
-        for(int i = 0; i < nums.length; i++) {
-            if(nums[i] == 0) {
-                countZero++;
+        int low = 0, mid = 0, high = nums.length-1;
+        while(mid <= high) {
+            if(nums[mid] == 0) {
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
             }
-            else if(nums[i] == 1) {
-                countOne++;
+            else if(nums[mid] == 1){
+                mid++;
             }
-            else if(nums[i] == 2) {
-                countTwo++;
+            else {
+                int temp = nums[high];
+                nums[high] = nums[mid];
+                nums[mid] = temp;
+                high--;
             }
-        }
-        int j = 0;
-        while(countZero > 0) {
-            nums[j] = 0;
-            j++;
-            countZero--;
-        }
-         while(countOne > 0) {
-            nums[j] = 1;
-            j++;
-             countOne--;
-        }
-         while(countTwo > 0) {
-            nums[j] = 2;
-            j++;
-            countTwo--;
         }
     }
+    
 }
